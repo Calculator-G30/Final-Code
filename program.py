df4425563290ab6f4ff1c2bfedd5e74cb47b4b60
Rahul Kumar <Rahul.Kumar20@st.niituniversity.in>
2023-03-02 13:01:42 +0530

    Version 1.0.3

--- program.py
+++ program.py
@@ -1,11 +1,14 @@
+
 # This function subtracts two numbers
 def subtract(x, y):
     return x - y
+# This function adds two numbers
+def add(x, y):
+    return x + y
 
 # This function multiplies two numbers
 def multiply(x, y):
     return x * y
-
 
 
 # This function divides two numbers
