951039f4796ba2b5340bd32c032ede8ba4dd6192
Rahul Kumar <Rahul.Kumar20@st.niituniversity.in>
2023-03-02 13:07:59 +0530

    Version 1.0.3

--- program.py
+++ program.py
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 # This function adds two numbers
 def add(x, y):
     return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
 
 # This function multiplies two numbers
 def multiply(x, y):
